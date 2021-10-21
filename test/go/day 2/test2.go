77dca7f7d153a0d1cd0268c52ab8cafa49f6a8ba
CNFisherman <1114211629@qq.com>
2021-10-21 13:35:16 +0800

    Update test2.go

--- test/go/day 2/test2.go
+++ test/go/day 2/test2.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+// 遍历字符串
+func traversalString() {
+	s := "hello沙河"
+	for i := 0; i < len(s); i++ { //byte
+		fmt.Printf("%v(%c) ", s[i], s[i])
+	}
+	fmt.Println("\n")
+	for _, r := range s { //rune
+		fmt.Printf("%v(%c) ", r, r)
+	}
+	fmt.Println()
+}
+
 func main() {
-	fmt.Printf("str := \"c:\\Code\\lesson1\\go.exe\"")\\转义符
+	fmt.Printf("str := \"c:\\Code\\lesson1\\go.exe\"\n")
+	traversalString()
 
 }

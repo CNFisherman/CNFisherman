bd8ffb565fea0c9a2af723efb62b7111124a42e8
CNFisherman <1114211629@qq.com>
2021-10-22 20:06:42 +0800

    Create test2.go

---
+++ test/go/day 3/test2/test2.go
@@ -0,0 +1,30 @@
+//分支结构
+package main
+
+import "fmt"
+
+func ifDemo1() {
+	score := 65
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score > 75 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+}
+
+func ifDemo2() {
+	if score := 65; score >= 90 {
+		fmt.Println("A")
+	} else if score > 75 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+}
+
+func main() {
+    ifDemo1()
+	ifDemo2()
+}
